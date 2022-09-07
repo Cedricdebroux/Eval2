3f551a421998658e7e8066c524d1ac5fff1e7c4c
Cedric Debroux <cedric@debroux.ovh>
2022-09-07 10:59:12 +0200

    Optimisation de code

--- app/src/main/java/com/technipixl/eval2/BloggiesFragment.kt
+++ app/src/main/java/com/technipixl/eval2/BloggiesFragment.kt
@@ -31,50 +31,10 @@ class BloggiesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mdp1 = binding.editTextTextPassword2.text
-        val mdp2 = binding.editTextTextPassword3.text
-        val login = binding.editTextTextPersonName.text
+
+
         binding.buttonInscription.setOnClickListener {
-            if (!isValidEmail(binding.editTextTextEmailAddress2.text)){
-                val builder = AlertDialog.Builder(context)
-                with(builder) {
-                    setTitle("Erreur")
-                    setMessage("Email Incomplet")
-                    setPositiveButton("OK", object: DialogInterface.OnClickListener{
-                        override fun onClick(dialog: DialogInterface?, which: Int) {
-                        }
-                    })
-
-                }.show()
-            }
-            if (binding.editTextTextPersonName.text.isEmpty()){
-                val builder = AlertDialog.Builder(context)
-                with(builder) {
-                    setTitle("Erreur")
-                    setMessage("Le login n'a pas été rempli.")
-                    setPositiveButton("OK", object: DialogInterface.OnClickListener{
-                        override fun onClick(dialog: DialogInterface?, which: Int) {
-                        }
-                    })
-
-                }.show()
-            }
-            if(mdp1 == mdp2){
-                val builder = AlertDialog.Builder(context)
-                with(builder) {
-                    setTitle("Erreur")
-                    setMessage("Les mots de passe ne correspondent pas")
-                    setPositiveButton("OK", object: DialogInterface.OnClickListener{
-                        override fun onClick(dialog: DialogInterface?, which: Int) {
-                        }
-                    })
-
-                }.show()
-            } else {
-                val direction = BloggiesFragmentDirections.actionBloggiesFragmentToBoogleConnectFragment("Bonjour $login")
-                findNavController().navigate(direction)
-            }
-
+            inscriptionOnClick()
         }
     }
     fun isValidEmail(target: CharSequence): Boolean {
@@ -83,5 +43,69 @@ class BloggiesFragment : Fragment() {
         } else {
             Patterns.EMAIL_ADDRESS.matcher(target).matches()
         }
+    }
+
+    fun inscriptionOnClick(){
+        if (!isValidEamil()) return
+        if (!isLoginValid()) return
+        if (!isPasswordNotEmpty()) return
+        if (!isSamePassword()) return
+
+         else {
+
+            val login = binding.editTextTextPersonName.text.toString()
+            val direction = BloggiesFragmentDirections.actionBloggiesFragmentToBoogleConnectFragment("Bonjour $login")
+            findNavController().navigate(direction)
+        }
+    }
+
+    fun isLoginValid(): Boolean{
+        if (binding.editTextTextPersonName.text.isEmpty()){
+            showErrorDialog("Erreur","Le login n'a pas été rempli.")
+            return false
+        }
+        return true
+    }
+
+    fun isSamePassword(): Boolean{
+        val mdp1 = binding.editTextTextPassword2.text.toString()
+        val mdp2 = binding.editTextTextPassword3.text.toString()
+        if(mdp1 != mdp2){
+            showErrorDialog("Erreur", "Les mots de passe ne correspondent pas")
+            return false
+        }
+        return true
+    }
+
+    fun isPasswordNotEmpty(): Boolean{
+        val mdp1 = binding.editTextTextPassword2.text.toString()
+        val mdp2 = binding.editTextTextPassword3.text.toString()
+        if(mdp1.isEmpty() && mdp2.isEmpty()){
+            showErrorDialog("Erreur", "Les champs mots de passe ne peuvent pas être vide")
+            return false
+        }
+        return true
+    }
+
+    fun isValidEamil():Boolean{
+        if (!isValidEmail(binding.editTextTextEmailAddress2.text)){
+            showErrorDialog("Erreur","Email Incomplet")
+            return false
+        }
+        return true
+    }
+
+    fun showErrorDialog(title : String, errorMessage : String){
+        val builder = AlertDialog.Builder(context)
+
+        with(builder) {
+            setTitle(title)
+            setMessage(errorMessage)
+            setPositiveButton("OK", object: DialogInterface.OnClickListener{
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                }
+            })
+
+        }.show()
     }
 }
